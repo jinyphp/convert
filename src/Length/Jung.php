1b2f0aadf34413a5ce66fa240411acedbe16b9e7
BeomjunSon <qjaens2321@gmail.com>
2019-06-12 20:25:52 +0900

    jung construct

--- src/Length/Jung.php
+++ src/Length/Jung.php
@@ -4,5 +4,15 @@
 namespace Jiny\Convert\Length;
 
 class Jung {
-    
+    private $value;
+
+    /**
+     * 초기화
+     */
+    public function __construct($value) {
+        $this->value = $value;
+    }
+    public function __invoke() {
+        return $this->value;
+    }
 }
