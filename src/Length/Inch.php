b0e8ebc37b7daef37b3b2b46811960998ba34f36
shy1008 <lokixa@naver.com>
2019-06-12 20:39:05 +0900

    inch

--- src/Length/Inch.php
+++ src/Length/Inch.php
@@ -4,21 +4,22 @@ namespace  Jiny\Convert\Length;
 class Inch
 {
     private $value;
-
+    
     // $value
-
-//    public function __construct()
-//    {
-//        return $this->value * 3.97;
-//    }
-
-public function __construct($value)
-   {
+    // public function __construct()
+    //  {
+    //    return $this->value * 39.97;    
+    //  }
+    public function __construct($value)
+     {
        $this->value = $value;
-   }
-
-public function __invoke()
-   {
-    return $this->value;
-   }
+     }
+    public function __invoke()
+     {
+       return $this->value;
+     }
+    public function inch()
+     {
+       return $this->value * 2.54;
+     }
 }
