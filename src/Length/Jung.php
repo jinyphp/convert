45c6ccaa1c4239876fb02dbb6e59ec83ef1c0513
BeomjunSon <qjaens2321@gmail.com>
2019-06-12 20:42:52 +0900

    add jung unit

--- src/Length/Jung.php
+++ src/Length/Jung.php
@@ -12,7 +12,43 @@ class Jung {
     public function __construct($value) {
         $this->value = $value;
     }
+    
     public function __invoke() {
         return $this->value;
+    }
+
+    /**
+     * 미터 변환
+     */
+    public function meter() {
+        return $this->value * 109.090909;
+    }
+    
+    /**
+     * 인치 변환
+     */
+    public function inch() {
+        return $this->value * 4294.91768;
+    }
+
+    /**
+     * 간 변환
+     */
+    public function gan() {
+        return $this->value * 60;
+    }
+
+    /**
+     * 피트 변환
+     */
+    public function feet() {
+        return $this->value * 357.909807;
+    }
+
+    /**
+     * 푸트 변환
+     */
+    public function foot() {
+        return $this->value * 357.909807;
     }
 }
