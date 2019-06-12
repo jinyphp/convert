7d9a587b3520e9fc89e3e4effa6ed028565661d1
hojinlee <infohojin@gmail.com>
2019-06-12 20:22:10 +0900

    meter construct

--- src/Length/Meter.php
+++ src/Length/Meter.php
@@ -5,5 +5,18 @@ namespace Jiny\Convert\Length;
 
 class Meter
 {
+    private $value;
+    
+    /**
+     * 초기화
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
 
+    public function __invoke()
+    {
+        return $this->value; 
+    }
 }
