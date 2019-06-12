e9c994723e6de8b83c520a3504a2c3babe4f7655
shy1008 <lokixa@naver.com>
2019-06-12 20:50:40 +0900

    inch

--- src/Length/Inch.php
+++ src/Length/Inch.php
@@ -4,22 +4,25 @@ namespace  Jiny\Convert\Length;
 class Inch
 {
     private $value;
-    
+
     // $value
     // public function __construct()
     //  {
     //    return $this->value * 39.97;    
     //  }
     public function __construct($value)
-     {
+    {
        $this->value = $value;
-     }
+    }
+    
     public function __invoke()
-     {
+    {
        return $this->value;
-     }
-    public function inch()
-     {
+    }
+
+    public function centi()
+    {
        return $this->value * 2.54;
-     }
+    }
+
 }
