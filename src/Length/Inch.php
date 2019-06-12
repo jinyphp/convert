db62a8d94db9fe08327708f4a0077901d25860b6
shy1008 <lokixa@naver.com>
2019-06-12 19:45:06 +0900

    add Inch

---
+++ src/Length/Inch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace  Jiny\Convert\Length;
+
+class Inch
+{
+   public function __construct()
+   {
+       return $this->value * 3.97;
+   }
+}
---
+++ src/Length/Meter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace  Jiny\Convert\Length;
+
+
+
+class Inch
+{
+   public function __construct()
+   {
+       return $this->value * 3.97;
+   }
+}
