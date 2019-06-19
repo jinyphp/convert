a82c88f9b4e786458afacca8526d3a1256d573ad
LEE2525 <48500264+LEE2525@users.noreply.github.com>
2019-06-19 18:08:35 +0900

    foot (#33)
    * foot
    
    * fix syntax error

--- src/Length/Foot.php
+++ src/Length/Foot.php
@@ -5,13 +5,45 @@ namespace Jiny\Convert\Length;
 class Foot
 {
     private $value;
+    
     public function __construct($value)
-{
-    $this->value = $value;
-}
+    {
+        $this->value = $value;
+    }
 
     public function __invoke()
     {
-    return $this->value;
+        return $this->value;
+    }
+
+    public function centi()
+    {   
+        return $this->value * 30.48;
+    }
+
+    public function feet()
+    {   
+        return $this->value;
+    }
+
+    public function meter()
+    {   
+        return $this->value * 0.3048;
+    }
+    public function inch()
+    {   
+        return $this->value * 12;
+    }
+    public function jung()
+    {   
+        return $this->value * 0.002794;
+    }
+    public function gan()
+    {   
+        return $this->value * 0.16764;
+    }
+    public function lee()
+    {   
+        return $this->value * 0.000776;
     }
 }
