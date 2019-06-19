3987832e0a290fcc0b721f4ec2787139de51b405
shy1008 <lokixa@naver.com>
2019-06-19 18:02:38 +0900

    inch 완료

--- src/Length/Inch.php
+++ src/Length/Inch.php
@@ -25,4 +25,32 @@ class Inch
        return $this->value * 2.54;
     }
 
+    public function meter()
+    {
+       return $this->value * 0.0254;
+    }
+
+    public function feet()
+    {
+       return $this->value * 0.083333;
+    }
+
+    public function foot()
+    {
+       return $this->value * 0.08382;
+    }
+
+    public function jung()
+    {
+       return $this->value * 0.000233;
+    }
+    public function gan()
+    {
+       return $this->value * 0.01397;
+    }
+
+   
+
+    
+
 }
