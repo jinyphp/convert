45545eb4005e64a9a752b0852a039075de8bf70e
pkd5064 <46986115+pkd5064@users.noreply.github.com>
2019-07-01 14:15:31 +0900

    압력변환 만들었습니다. (#49)
    * add pressure
    
    * add pressure

---
+++ src/Pressure/hPa.php
@@ -0,0 +1,41 @@
+<?php
+// 압력변환
+
+namespace Jiny\Convert\Pressure;
+
+use Jiny\Convert\Unit;
+
+class hPa extends Unit
+{
+    /**
+     * 객체 타입 상수지정
+     */
+    const TYPE="hPa";
+    
+    /**
+     * 초기화
+     */
+    public function __construct($value=null)
+    {
+        if ($value) {
+            $this->value = $value;
+        }
+    }
+
+    public function __invoke()
+    {
+        return $this->value;
+    }
+
+    public function atm($value=null)
+    {
+        return $this->conv($value, 0.000987);
+    }
+
+    public function Pa($value=null)
+    {
+        return $this->conv($value, 100);
+    }
+
+   
+}
