77a82099bc72e31f9f66b0e64f14016d40f524ae
BeomjunSon <48266046+BeomjunSon@users.noreply.github.com>
2019-07-01 12:58:36 +0900

    Add weight value (#46)
    * add jung unit
    
    * add&fix jung unit
    
    * fix jung unit
    
    * add test jung
    
    * add singleturn in Jung.php and add Weight folder
    
    * fix Jung.php
    
    * move Jung.php
    
    * add Weight value

--- src/Length/Jung.php
+++ src/Length/Jung.php
@@ -6,27 +6,11 @@ namespace Jiny\Convert\Length;
 use Jiny\Convert\Unit;
 
 class Jung extends Unit{
-    protected $value;
 
     /**
      * 객체 타입 상수지정
      */
     const TYPE="Jung";
-
-    public function type() {
-        return self::TYPE;
-    }
-
-    /**
-     * 싱글턴
-     */
-    private static $Instance;
-    public static function instance() {
-        if (!isset(self::$Instance)) {
-            self::$Instance = new self();
-        }
-        return self::$Instance;
-    }
 
     /**
      * 초기화
@@ -39,20 +23,6 @@ class Jung extends Unit{
 
     public function __invoke() {
         return $this->value; 
-    }
-
-    /**
-     * 값 지정
-     */
-    public function setValue($value) {
-        $this->value = $value;
-    }
-
-    /**
-     * 값 출력
-     */
-    public function getValue() {
-        return $this->value;
     }
 
     /**
